fb3f8f2177e945396e2655f97597812cb4d3212e
Robbie Cook <robbieck7@gmail.com>
2020-08-02 02:05:07 +1200

    Started adding router

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@
 import { css, jsx } from "@emotion/core";
 import "./App.css";
 
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import IndexPage from "./pages/index";
 
 function App() {
   return (
     <div className="App">
-      <IndexPage />
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <IndexPage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
